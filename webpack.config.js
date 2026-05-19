23fbd99b6e0012724d1555aea11c44dd9b42433e
Rudik Manukyan <rudik770@gmail.com>
2026-05-19 17:43:10 +0400

    fix env

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html'
     }),
-    new Dotenv()
+    new Dotenv({
+      systemvars: true,
+      silent: true
+    })
   ],
   resolve: {
     extensions: [
